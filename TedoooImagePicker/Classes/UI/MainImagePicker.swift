7fb61f38efa509bdcf0b4d8e5db14e7d5a44de6d
morapelker <morapelker@gmail.com>
2022-07-03 16:27:21 +0300

    "v1.0.3"

--- TedoooImagePicker/Classes/UI/MainImagePicker.swift
+++ TedoooImagePicker/Classes/UI/MainImagePicker.swift
@@ -3,5 +3,5 @@ import Combine
 public protocol TedoooImagePicker {
     
     func pickImages(from: UIViewController, single: Bool, withCamera: Bool) -> AnyPublisher<UIImage?, Never>
-    
+    func editImage(from: UIViewController, image: UIImage) -> AnyPublisher<UIImage, Never>
 }
